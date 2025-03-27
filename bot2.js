9205bbd2a5d9283ac1bc94d4a9b062f74b77034c
Adkins <adkinsmwangi@gmail.com>
2025-03-27 14:51:28 +0300

    made another console bot

---
+++ bot2.js
@@ -0,0 +1,22 @@
+console.log("Hello! I'm your coding fun fact guide!")
+
+let botName = "Bradley";
+let botLocation = "a server";
+let favoriteLanguage = "HTML";
+
+const intro = "My name is " +botName+ " and I live on " +botLocation+ "."
+console.log(intro)
+
+const lang = "My favorite programming language is " +favoriteLanguage+ "."
+console.log(lang)
+
+let codingFact = favoriteLanguage+ " was invented by Tim Berners-Lee."
+console.log(codingFact)
+
+codingFact = favoriteLanguage+ " is the most prefferred when making websites."
+console.log(codingFact)
+
+codingFact = favoriteLanguage+ " works in conjuction with CSS and Javascript to make a website functional."
+console.log(codingFact)
+
+console.log("It was fun sharing these facts with you. Goodbye! - " +botName+ " from " +botLocation+ ".")
